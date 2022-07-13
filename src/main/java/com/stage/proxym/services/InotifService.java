9a032b366e4680a46dd0d3778e1763a53ddcaf03
Eya KAABACHI <eya.kaabachi@esprit.tn>
2022-07-13 15:50:26 +0100

    notifications

--- src/main/java/com/stage/proxym/services/InotifService.java
+++ src/main/java/com/stage/proxym/services/InotifService.java
@@ -1,4 +1,12 @@
 package com.stage.proxym.services;
 
+import com.stage.proxym.entities.Notification;
+
+import java.util.List;
+
 public interface InotifService {
+    public Notification addNotification(Notification notif);
+    public Notification getNotificationById(long idNotification);
+    public List<Notification> getAll();
+    public List<Notification> retrieveAllNotification();
 }
--- src/main/java/com/stage/proxym/services/NotifService.java
+++ src/main/java/com/stage/proxym/services/NotifService.java
@@ -1,7 +1,37 @@
 package com.stage.proxym.services;
 
+import com.stage.proxym.entities.Income;
+import com.stage.proxym.entities.Notification;
+import com.stage.proxym.repositories.NotificationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+
 @Service
-public class NotifService {
+public class NotifService implements InotifService {
+
+    @Autowired
+    NotificationRepository notifrepo;
+    @Override
+    public Notification addNotification(Notification notif) {
+        return notifrepo.save(notif);
+    }
+
+    @Override
+    public Notification getNotificationById(long idNotification) {
+        return notifrepo.findById(idNotification).orElse(null);
+    }
+
+    @Override
+    public List<Notification> getAll() {
+        return notifrepo.findAll();
+    }
+
+    @Override
+    public List<Notification> retrieveAllNotification() {
+        List<Notification> notifications = (List<Notification>)notifrepo.findAll();
+        return notifications;
+    }
 }
